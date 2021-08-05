cf61eff8b0da5dd98e1bebdc049cea60d36302e5
Oscar Espinosa <oscarzx@gmail.com>
2021-08-04 20:30:09 -0500

    DCliente

---
+++ CapaDatos/DCliente.cs
@@ -0,0 +1,470 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CapaDatos
+{
+    public class DCliente
+    {
+        private int idCliente;
+
+        public int IdCliente
+        {
+            get { return idCliente; }
+            set { idCliente = value; }
+        }
+
+        private string nombre;
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        private string apellido;
+
+        public string Apellido
+        {
+            get { return apellido; }
+            set { apellido = value; }
+        }
+
+        private string genero;
+
+        public string Genero
+        {
+            get { return genero; }
+            set { genero = value; }
+        }
+
+        private DateTime fecha;
+
+        public DateTime Fecha
+        {
+            get { return fecha; }
+            set { fecha = value; }
+        }
+
+        private string tipoDocumento;
+
+        public string TipoDocumento
+        {
+            get { return tipoDocumento; }
+            set { tipoDocumento = value; }
+        }
+
+        private string numeroDocumento;
+
+        public string NumeroDocumento
+        {
+            get { return numeroDocumento; }
+            set { numeroDocumento = value; }
+        }
+
+        private string direccion;
+
+        public string Direccion
+        {
+            get { return direccion; }
+            set { direccion = value; }
+        }
+
+        private string telefono;
+
+        public string Telefono
+        {
+            get { return telefono; }
+            set { telefono = value; }
+        }
+
+        private string email;
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        private string textoBuscar;
+
+        public string TextoBuscar
+        {
+            get { return textoBuscar; }
+            set { textoBuscar = value; }
+        }
+
+
+        public DCliente()
+        {
+
+        }
+
+        public DCliente(int idcliente, string nombre, string apellido, string genero, DateTime fecha, string tipodocumento,
+            string numerodocumento, string direccion, string telefono, string email)
+        {
+            this.Apellido = apellido;
+            this.Direccion = direccion;
+            this.Email = email;
+            this.Fecha = fecha;
+            this.Genero = genero;
+            this.IdCliente = idcliente;
+            this.Nombre = nombre;
+            this.NumeroDocumento = numerodocumento;
+            this.Telefono = telefono;
+            this.TipoDocumento = tipodocumento;
+        }
+
+        //Método Insertar
+        public string Insertar(DCliente Cliente)
+        {
+            string rpta = "";
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                //Código
+                SqlCon.ConnectionString = Conexion.Cn;
+                SqlCon.Open();
+
+                //Establecer comando
+                SqlCommand Sqlcmd = new SqlCommand();
+                Sqlcmd.Connection = SqlCon;
+                Sqlcmd.CommandText = "spinsertar_cliente";
+                Sqlcmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter ParIdcliente = new SqlParameter();
+                ParIdcliente.ParameterName = "@idcliente";
+                ParIdcliente.SqlDbType = SqlDbType.Int;
+                ParIdcliente.Direction = ParameterDirection.Output;
+                Sqlcmd.Parameters.Add(ParIdcliente);
+
+                SqlParameter ParNombre = new SqlParameter();
+                ParNombre.ParameterName = "@nombre_cliente";
+                ParNombre.SqlDbType = SqlDbType.VarChar;
+                ParNombre.Size = 50;
+                ParNombre.Value = Cliente.Nombre;
+                Sqlcmd.Parameters.Add(ParNombre);
+
+                SqlParameter ParApellido = new SqlParameter();
+                ParApellido.ParameterName = "@apellido_cliente";
+                ParApellido.SqlDbType = SqlDbType.VarChar;
+                ParApellido.Size = 40;
+                ParApellido.Value = Cliente.Apellido;
+                Sqlcmd.Parameters.Add(ParApellido);
+
+                SqlParameter ParGenero = new SqlParameter();
+                ParGenero.ParameterName = "@genero";
+                ParGenero.SqlDbType = SqlDbType.VarChar;
+                ParGenero.Size = 1;
+                ParGenero.Value = Cliente.Genero;
+                Sqlcmd.Parameters.Add(ParGenero);
+
+                SqlParameter ParFechaNacimiento = new SqlParameter();
+                ParFechaNacimiento.ParameterName = "@fecha_nacimiento";
+                ParFechaNacimiento.SqlDbType = SqlDbType.DateTime;
+                ParFechaNacimiento.Value = Cliente.Fecha;
+                Sqlcmd.Parameters.Add(ParFechaNacimiento);
+
+                SqlParameter ParTipoDocumento = new SqlParameter();
+                ParTipoDocumento.ParameterName = "@tipo_documento";
+                ParTipoDocumento.SqlDbType = SqlDbType.VarChar;
+                ParTipoDocumento.Size = 20;
+                ParTipoDocumento.Value = Cliente.TipoDocumento;
+                Sqlcmd.Parameters.Add(ParTipoDocumento);
+
+                SqlParameter ParNumDocumento = new SqlParameter();
+                ParNumDocumento.ParameterName = "@numero_documento";
+                ParNumDocumento.SqlDbType = SqlDbType.VarChar;
+                ParNumDocumento.Size = 12;
+                ParNumDocumento.Value = Cliente.NumeroDocumento;
+                Sqlcmd.Parameters.Add(ParNumDocumento);
+
+                SqlParameter ParDireccion = new SqlParameter();
+                ParDireccion.ParameterName = "@direccion";
+                ParDireccion.SqlDbType = SqlDbType.VarChar;
+                ParDireccion.Size = 100;
+                ParDireccion.Value = Cliente.Direccion;
+                Sqlcmd.Parameters.Add(ParDireccion);
+
+                SqlParameter ParTelefono = new SqlParameter();
+                ParTelefono.ParameterName = "@telefono";
+                ParTelefono.SqlDbType = SqlDbType.VarChar;
+                ParTelefono.Size = 10;
+                ParTelefono.Value = Cliente.Telefono;
+                Sqlcmd.Parameters.Add(ParTelefono);
+
+                SqlParameter ParEmail = new SqlParameter();
+                ParEmail.ParameterName = "@email";
+                ParEmail.SqlDbType = SqlDbType.VarChar;
+                ParEmail.Size = 50;
+                ParEmail.Value = Cliente.Email;
+                Sqlcmd.Parameters.Add(ParEmail);
+
+                //Ejecutamos nuestro comando
+                rpta = Sqlcmd.ExecuteNonQuery() == 1 ? "OK" : "No se ingresó el registro";
+
+            }
+            catch (Exception ex)
+            {
+                rpta = ex.Message;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+            return rpta;
+        }
+
+        public string Editar(DCliente Cliente)
+        {
+            string rpta = "";
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                //Código
+                SqlCon.ConnectionString = Conexion.Cn;
+                SqlCon.Open();
+
+                //Establecer comando
+                SqlCommand Sqlcmd = new SqlCommand();
+                Sqlcmd.Connection = SqlCon;
+                Sqlcmd.CommandText = "speditar_cliente";
+                Sqlcmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter ParIdcliente = new SqlParameter();
+                ParIdcliente.ParameterName = "@idcliente";
+                ParIdcliente.SqlDbType = SqlDbType.Int;
+                ParIdcliente.Value = Cliente.IdCliente;
+                Sqlcmd.Parameters.Add(ParIdcliente);
+
+                SqlParameter ParNombre = new SqlParameter();
+                ParNombre.ParameterName = "@nombre_cliente";
+                ParNombre.SqlDbType = SqlDbType.VarChar;
+                ParNombre.Size = 50;
+                ParNombre.Value = Cliente.Nombre;
+                Sqlcmd.Parameters.Add(ParNombre);
+
+                SqlParameter ParApellido = new SqlParameter();
+                ParApellido.ParameterName = "@apellido_cliente";
+                ParApellido.SqlDbType = SqlDbType.VarChar;
+                ParApellido.Size = 40;
+                ParApellido.Value = Cliente.Apellido;
+                Sqlcmd.Parameters.Add(ParApellido);
+
+                SqlParameter ParGenero = new SqlParameter();
+                ParGenero.ParameterName = "@genero";
+                ParGenero.SqlDbType = SqlDbType.VarChar;
+                ParGenero.Size = 1;
+                ParGenero.Value = Cliente.Genero;
+                Sqlcmd.Parameters.Add(ParGenero);
+
+                SqlParameter ParFechaNacimiento = new SqlParameter();
+                ParFechaNacimiento.ParameterName = "@fecha_nacimiento";
+                ParFechaNacimiento.SqlDbType = SqlDbType.DateTime;
+                ParFechaNacimiento.Value = Cliente.Fecha;
+                Sqlcmd.Parameters.Add(ParFechaNacimiento);
+
+                SqlParameter ParTipoDocumento = new SqlParameter();
+                ParTipoDocumento.ParameterName = "@tipo_documento";
+                ParTipoDocumento.SqlDbType = SqlDbType.VarChar;
+                ParTipoDocumento.Size = 20;
+                ParTipoDocumento.Value = Cliente.TipoDocumento;
+                Sqlcmd.Parameters.Add(ParTipoDocumento);
+
+                SqlParameter ParNumDocumento = new SqlParameter();
+                ParNumDocumento.ParameterName = "@numero_documento";
+                ParNumDocumento.SqlDbType = SqlDbType.VarChar;
+                ParNumDocumento.Size = 12;
+                ParNumDocumento.Value = Cliente.NumeroDocumento;
+                Sqlcmd.Parameters.Add(ParNumDocumento);
+
+                SqlParameter ParDireccion = new SqlParameter();
+                ParDireccion.ParameterName = "@direccion";
+                ParDireccion.SqlDbType = SqlDbType.VarChar;
+                ParDireccion.Size = 100;
+                ParDireccion.Value = Cliente.Direccion;
+                Sqlcmd.Parameters.Add(ParDireccion);
+
+                SqlParameter ParTelefono = new SqlParameter();
+                ParTelefono.ParameterName = "@telefono";
+                ParTelefono.SqlDbType = SqlDbType.VarChar;
+                ParTelefono.Size = 10;
+                ParTelefono.Value = Cliente.Telefono;
+                Sqlcmd.Parameters.Add(ParTelefono);
+
+                SqlParameter ParEmail = new SqlParameter();
+                ParEmail.ParameterName = "@email";
+                ParEmail.SqlDbType = SqlDbType.VarChar;
+                ParEmail.Size = 50;
+                ParEmail.Value = Cliente.Email;
+                Sqlcmd.Parameters.Add(ParEmail);
+
+                //Ejecutamos nuestro comando
+                rpta = Sqlcmd.ExecuteNonQuery() == 1 ? "OK" : "No se actualizó el registro";
+            }
+            catch (Exception ex)
+            {
+                rpta = ex.Message;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+            return rpta;
+        }
+
+        public string Eliminar(DCliente Cliente)
+        {
+            string rpta = "";
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                //Código
+                SqlCon.ConnectionString = Conexion.Cn;
+                SqlCon.Open();
+
+                //Establecer comando
+                SqlCommand Sqlcmd = new SqlCommand();
+                Sqlcmd.Connection = SqlCon;
+                Sqlcmd.CommandText = "speliminar_cliente";
+                Sqlcmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter ParIdproveedor = new SqlParameter();
+                ParIdproveedor.ParameterName = "@IdProveedor";
+                ParIdproveedor.SqlDbType = SqlDbType.Int;
+                ParIdproveedor.Value = Cliente.IdCliente;
+                Sqlcmd.Parameters.Add(ParIdproveedor);
+
+                //Ejecutamos nuestro comando
+                rpta = Sqlcmd.ExecuteNonQuery() == 1 ? "OK" : "No se eliminó el registro";
+            }
+            catch (Exception ex)
+            {
+                rpta = ex.Message;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+            return rpta;
+        }
+
+        public DataTable Mostrar()
+        {
+            DataTable DtResultado = new DataTable("Cliente");
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                SqlCon.ConnectionString = Conexion.Cn;
+                SqlCommand SqlCmd = new SqlCommand();
+                SqlCmd.Connection = SqlCon;
+                SqlCmd.CommandText = "spmostar_cliente";
+                SqlCmd.CommandType = CommandType.StoredProcedure;
+
+                SqlDataAdapter SqlDat = new SqlDataAdapter(SqlCmd);
+                SqlDat.Fill(DtResultado);
+            }
+            catch (Exception ex)
+            {
+                DtResultado = null;
+
+            }
+            return DtResultado;
+        }
+
+        public DataTable BuscarCliente_Documento(DCliente Cliente)
+        {
+            DataTable DtResultado = new DataTable("Cliente");
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                SqlCon.ConnectionString = Conexion.Cn;
+                SqlCommand SqlCmd = new SqlCommand();
+                SqlCmd.Connection = SqlCon;
+                SqlCmd.CommandText = "sobuscar_cliente_documento";
+                SqlCmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter ParClienteDocumento = new SqlParameter();
+                ParClienteDocumento.ParameterName = "@textoBuscar";
+                ParClienteDocumento.SqlDbType = SqlDbType.VarChar;
+                ParClienteDocumento.Size = 50;
+                ParClienteDocumento.Value = Cliente.TextoBuscar;
+                SqlCmd.Parameters.Add(ParClienteDocumento);
+
+                SqlDataAdapter SqlDat = new SqlDataAdapter(SqlCmd);
+                SqlDat.Fill(DtResultado);
+            }
+            catch (Exception ex)
+            {
+                DtResultado = null;
+
+            }
+            return DtResultado;
+        }
+
+        public DataTable BuscarNum_Documento(DCliente Cliente)
+        {
+            DataTable DtResultado = new DataTable("Cliente");
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                SqlCon.ConnectionString = Conexion.Cn;
+                SqlCommand SqlCmd = new SqlCommand();
+                SqlCmd.Connection = SqlCon;
+                SqlCmd.CommandText = "spbuscar_proveedor_num_documento";
+                SqlCmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter ParTextoBuscar = new SqlParameter();
+                ParTextoBuscar.ParameterName = "@textoBuscar";
+                ParTextoBuscar.SqlDbType = SqlDbType.VarChar;
+                ParTextoBuscar.Size = 50;
+                ParTextoBuscar.Value = Cliente.TextoBuscar;
+                SqlCmd.Parameters.Add(ParTextoBuscar);
+
+                SqlDataAdapter SqlDat = new SqlDataAdapter(SqlCmd);
+                SqlDat.Fill(DtResultado);
+            }
+            catch (Exception ex)
+            {
+                DtResultado = null;
+
+            }
+            return DtResultado;
+        }
+
+        public DataTable BuscarApellido(DCliente Cliente)
+        {
+            DataTable DtResultado = new DataTable("Cliente");
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                SqlCon.ConnectionString = Conexion.Cn;
+                SqlCommand SqlCmd = new SqlCommand();
+                SqlCmd.Connection = SqlCon;
+                SqlCmd.CommandText = "sobuscar_cliente_apellido";
+                SqlCmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter ParTextoBuscar = new SqlParameter();
+                ParTextoBuscar.ParameterName = "@textoBuscar";
+                ParTextoBuscar.SqlDbType = SqlDbType.VarChar;
+                ParTextoBuscar.Size = 50;
+                ParTextoBuscar.Value = Cliente.TextoBuscar;
+                SqlCmd.Parameters.Add(ParTextoBuscar);
+
+                SqlDataAdapter SqlDat = new SqlDataAdapter(SqlCmd);
+                SqlDat.Fill(DtResultado);
+            }
+            catch (Exception ex)
+            {
+                DtResultado = null;
+
+            }
+            return DtResultado;
+        }
+
+    }
+}
