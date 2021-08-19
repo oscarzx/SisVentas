fc20206791571f48afd480d296e2cdc98149ae93
Oscar Espinosa <oscarzx@gmail.com>
2021-08-18 22:00:28 -0500

    clases detalle_venta, venta

---
+++ CapaDatos/DDetalle_venta.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CapaDatos
+{
+    public class DDetalle_venta
+    {
+        private int idDetalle_venta;
+
+        public int IdDetalle_venta
+        {
+            get { return idDetalle_venta; }
+            set { idDetalle_venta = value; }
+        }
+
+        private int idVenta;
+
+        public int IdVenta
+        {
+            get { return idVenta; }
+            set { idVenta = value; }
+        }
+
+        private int idDetalle_ingreso;
+
+        public int IdDetalle_ingreso
+        {
+            get { return idDetalle_ingreso; }
+            set { idDetalle_ingreso = value; }
+        }
+
+        private float cantidad;
+
+        public float Cantidad
+        {
+            get { return cantidad; }
+            set { cantidad = value; }
+        }
+
+        private decimal precio_venta;
+
+        public decimal Precio_venta
+        {
+            get { return precio_venta; }
+            set { precio_venta = value; }
+        }
+
+        private decimal descuento;
+
+        public decimal Descuento
+        {
+            get { return descuento; }
+            set { descuento = value; }
+        }
+
+
+        public DDetalle_venta()
+        {
+
+        }
+
+        public DDetalle_venta(int _idDetalle_venta, int _idVenta, int _idDetalle_ingreso, float _cantidad,
+            decimal _precio_venta, decimal _descuento)
+        {
+            this.Cantidad = _cantidad;
+            this.Descuento = _descuento;
+            this.IdDetalle_ingreso = _idDetalle_ingreso;
+            this.IdDetalle_venta = _idDetalle_venta;
+            this.IdVenta = _idVenta;
+            this.Precio_venta = _precio_venta;
+        }
+
+
+        //Método insertar
+        public string Insertar(DDetalle_venta Detalle_venta,
+            ref SqlConnection SqlCon, ref SqlTransaction sqlTransaction)
+        {
+            string rpta = "";
+
+            try
+            {
+                //Establecer comando
+                SqlCommand Sqlcmd = new SqlCommand();
+                Sqlcmd.Connection = SqlCon;
+                Sqlcmd.Transaction = sqlTransaction;
+                Sqlcmd.CommandText = "spinsertar_detalle_venta";
+                Sqlcmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter ParIdDetalle_venta = new SqlParameter();
+                ParIdDetalle_venta.ParameterName = "@iddetalle_venta";
+                ParIdDetalle_venta.SqlDbType = SqlDbType.Int;
+                ParIdDetalle_venta.Direction = ParameterDirection.Output;
+                Sqlcmd.Parameters.Add(ParIdDetalle_venta);
+
+                SqlParameter ParIdventa = new SqlParameter();
+                ParIdventa.ParameterName = "@idventa";
+                ParIdventa.SqlDbType = SqlDbType.Int;
+                ParIdventa.Value = Detalle_venta.IdVenta;
+                Sqlcmd.Parameters.Add(ParIdventa);
+
+                SqlParameter ParIdDetalle_ingreso = new SqlParameter();
+                ParIdDetalle_ingreso.ParameterName = "@iddetalle_ingreso";
+                ParIdDetalle_ingreso.SqlDbType = SqlDbType.Int;
+                ParIdDetalle_ingreso.Value = Detalle_venta.IdDetalle_ingreso;
+                Sqlcmd.Parameters.Add(ParIdDetalle_ingreso);
+
+                SqlParameter ParCantidad = new SqlParameter();
+                ParCantidad.ParameterName = "@cantidad";
+                ParCantidad.SqlDbType = SqlDbType.Float;
+                ParCantidad.Value = Detalle_venta.cantidad;
+                Sqlcmd.Parameters.Add(ParCantidad);
+
+                SqlParameter ParPrecio_venta = new SqlParameter();
+                ParPrecio_venta.ParameterName = "@precio_venta";
+                ParPrecio_venta.SqlDbType = SqlDbType.Money;
+                ParPrecio_venta.Value = Detalle_venta.Precio_venta;
+                Sqlcmd.Parameters.Add(ParPrecio_venta);
+
+                SqlParameter ParDescuento = new SqlParameter();
+                ParDescuento.ParameterName = "@descuento";
+                ParDescuento.SqlDbType = SqlDbType.Money;
+                ParDescuento.Value = Detalle_venta.Descuento;
+                Sqlcmd.Parameters.Add(ParDescuento);
+
+                //Ejecutamos nuestro comando
+                rpta = Sqlcmd.ExecuteNonQuery() == 1 ? "OK" : "No se ingresó el registro";
+
+
+            }
+            catch (Exception ex)
+            {
+                rpta = ex.Message;
+            }
+            return rpta;
+        }
+
+    }
+}
---
+++ CapaDatos/DVenta.cs
@@ -0,0 +1,460 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CapaDatos
+{
+    public class DVenta
+    {
+        private int idVenta;
+
+        public int IdVenta
+        {
+            get { return idVenta; }
+            set { idVenta = value; }
+        }
+
+        private int idCliente;
+
+        public int IdCliente
+        {
+            get { return idCliente; }
+            set { idCliente = value; }
+        }
+
+        private int idEmpleado;
+
+        public int IdEmpleado
+        {
+            get { return idEmpleado; }
+            set { idEmpleado = value; }
+        }
+
+        private DateTime fecha;
+
+        public DateTime Fecha
+        {
+            get { return fecha; }
+            set { fecha = value; }
+        }
+
+        private string tipo_comprobante;
+
+        public string Tipo_comprobante
+        {
+            get { return tipo_comprobante; }
+            set { tipo_comprobante = value; }
+        }
+
+        private string serie;
+
+        public string Serie
+        {
+            get { return serie; }
+            set { serie = value; }
+        }
+
+        private string correlativo;
+
+        public string Correlativo
+        {
+            get { return correlativo; }
+            set { correlativo = value; }
+        }
+
+        private decimal iva;
+
+        public decimal Iva
+        {
+            get { return iva; }
+            set { iva = value; }
+        }
+
+
+        public DVenta()
+        {
+
+        }
+
+        public DVenta(int _idVenta, int _idCliente, int _idEmpleado, DateTime _fecha,
+            string _tipo_comprobante, string _serie, string _correlativo, decimal _iva)
+        {
+            this.Correlativo = _correlativo;
+            this.Fecha = _fecha;
+            this.IdCliente = _idCliente;
+            this.IdEmpleado = _idEmpleado;
+            this.IdVenta = _idVenta;
+            this.Iva = _iva;
+            this.Serie = _serie;
+            this.Tipo_comprobante = _tipo_comprobante;
+        }
+
+        //Métodos
+        public string DisminuirStock(int idDetalle_ingreso, float cantidad)
+        {
+            string rpta = "";
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                //Código
+                SqlCon.ConnectionString = Conexion.Cn;
+                SqlCon.Open();
+
+                //Establecer comando
+                SqlCommand Sqlcmd = new SqlCommand();
+                Sqlcmd.Connection = SqlCon;
+                Sqlcmd.CommandText = "spdisminuir_stock";
+                Sqlcmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter ParIdDetalle_ingreso = new SqlParameter();
+                ParIdDetalle_ingreso.ParameterName = "@iddetalle_ingreso";
+                ParIdDetalle_ingreso.SqlDbType = SqlDbType.Int;
+                ParIdDetalle_ingreso.Value = idDetalle_ingreso;
+                Sqlcmd.Parameters.Add(ParIdDetalle_ingreso);
+
+                SqlParameter ParCantidad = new SqlParameter();
+                ParCantidad.ParameterName = "@cantidad";
+                ParCantidad.SqlDbType = SqlDbType.Int;
+                ParCantidad.Value = cantidad;
+                Sqlcmd.Parameters.Add(ParCantidad);
+
+                //Ejecutamos nuestro comando
+                rpta = Sqlcmd.ExecuteNonQuery() == 1 ? "OK" : "No se actualizó el stock";
+            }
+            catch (Exception ex)
+            {
+                rpta = ex.Message;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+            return rpta;
+        }
+
+
+        //Insertar
+        public string Insertar(DVenta Venta, List<DDetalle_venta> Detalle)
+        {
+            string rpta = "";
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                //Código
+                SqlCon.ConnectionString = Conexion.Cn;
+                SqlCon.Open();
+
+                //Establecer la transacción
+                SqlTransaction sqlTransaction = SqlCon.BeginTransaction();
+
+                //Establecer comando
+                SqlCommand Sqlcmd = new SqlCommand();
+                Sqlcmd.Connection = SqlCon;
+                Sqlcmd.Transaction = sqlTransaction;
+                Sqlcmd.CommandText = "spinsertar_venta";
+                Sqlcmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter ParIdVenta = new SqlParameter();
+                ParIdVenta.ParameterName = "@idventa";
+                ParIdVenta.SqlDbType = SqlDbType.Int;
+                ParIdVenta.Direction = ParameterDirection.Output;
+                Sqlcmd.Parameters.Add(ParIdVenta);
+
+                SqlParameter ParIdCliente = new SqlParameter();
+                ParIdCliente.ParameterName = "@idcliente";
+                ParIdCliente.SqlDbType = SqlDbType.Int;
+                ParIdCliente.Value = Venta.IdEmpleado;
+                Sqlcmd.Parameters.Add(ParIdCliente);
+
+                SqlParameter ParIdEmpleado = new SqlParameter();
+                ParIdEmpleado.ParameterName = "@idempleado";
+                ParIdEmpleado.SqlDbType = SqlDbType.Int;
+                ParIdEmpleado.Value = Venta.IdEmpleado;
+                Sqlcmd.Parameters.Add(ParIdEmpleado);
+
+                SqlParameter ParFecha = new SqlParameter();
+                ParFecha.ParameterName = "@fecha";
+                ParFecha.SqlDbType = SqlDbType.Date;
+                ParFecha.Value = Venta.Fecha;
+                Sqlcmd.Parameters.Add(ParFecha);
+
+                SqlParameter ParTipoComprobante = new SqlParameter();
+                ParTipoComprobante.ParameterName = "@tipo_comprobante";
+                ParTipoComprobante.SqlDbType = SqlDbType.VarChar;
+                ParTipoComprobante.Size = 20;
+                ParTipoComprobante.Value = Venta.Tipo_comprobante;
+                Sqlcmd.Parameters.Add(ParTipoComprobante);
+
+                SqlParameter ParSerie = new SqlParameter();
+                ParSerie.ParameterName = "@serie";
+                ParSerie.SqlDbType = SqlDbType.VarChar;
+                ParSerie.Size = 4;
+                ParSerie.Value = Venta.Serie;
+                Sqlcmd.Parameters.Add(ParSerie);
+
+                SqlParameter ParCorrelativo = new SqlParameter();
+                ParCorrelativo.ParameterName = "@correlativo";
+                ParCorrelativo.SqlDbType = SqlDbType.VarChar;
+                ParCorrelativo.Size = 7;
+                ParCorrelativo.Value = Venta.Correlativo;
+                Sqlcmd.Parameters.Add(ParCorrelativo);
+
+                SqlParameter ParIva = new SqlParameter();
+                ParIva.ParameterName = "@iva";
+                ParIva.SqlDbType = SqlDbType.Decimal;
+                ParIva.Precision = 4;
+                ParIva.Scale = 2;
+                ParIva.Value = Venta.Iva;
+                Sqlcmd.Parameters.Add(ParIva);
+
+                
+
+                //Ejecutamos nuestro comando
+                rpta = Sqlcmd.ExecuteNonQuery() == 1 ? "OK" : "No se ingresó el registro";
+
+                if (rpta.Equals("OK"))
+                {
+                    //Obtener el código del ingreso generado
+                    this.IdVenta = Convert.ToInt32(Sqlcmd.Parameters["@idventa"].Value);
+
+                    foreach (DDetalle_venta item in Detalle)
+                    {
+                        item.IdVenta = this.IdVenta;
+                        //Llamar al método insertar de la clase DDetalle_ingreso
+                        rpta = item.Insertar(item, ref SqlCon, ref sqlTransaction);
+
+                        if (!rpta.Equals("OK"))
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            //Actualizamos el stock
+                            rpta = DisminuirStock(item.IdDetalle_ingreso, item.Cantidad);
+                            if (!rpta.Equals("OK"))
+                            {
+                                break;
+                            }
+                        }
+                    }
+                }
+                if (rpta.Equals("OK"))
+                {
+                    sqlTransaction.Commit();
+                }
+                else
+                {
+                    sqlTransaction.Rollback();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                rpta = ex.Message;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+            return rpta;
+        }
+
+
+        //Eliminar
+        public string Eliminar(DVenta Venta)
+        {
+            string rpta = "";
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                //Código
+                SqlCon.ConnectionString = Conexion.Cn;
+                SqlCon.Open();
+
+                //Establecer comando
+                SqlCommand Sqlcmd = new SqlCommand();
+                Sqlcmd.Connection = SqlCon;
+                Sqlcmd.CommandText = "speliminar_venta";
+                Sqlcmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter ParIdventa = new SqlParameter();
+                ParIdventa.ParameterName = "@idventa";
+                ParIdventa.SqlDbType = SqlDbType.Int;
+                ParIdventa.Value = Venta.IdVenta;
+                Sqlcmd.Parameters.Add(ParIdventa);
+
+                //Ejecutamos nuestro comando
+                rpta = Sqlcmd.ExecuteNonQuery() == 1 ? "OK" : "OK";
+            }
+            catch (Exception ex)
+            {
+                rpta = ex.Message;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+            return rpta;
+        }
+
+
+        //Buscar Mostrar
+        public DataTable Mostrar()
+        {
+            DataTable DtResultado = new DataTable("Venta");
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                SqlCon.ConnectionString = Conexion.Cn;
+                SqlCommand SqlCmd = new SqlCommand();
+                SqlCmd.Connection = SqlCon;
+                SqlCmd.CommandText = "spmostrar_venta";
+                SqlCmd.CommandType = CommandType.StoredProcedure;
+
+                SqlDataAdapter SqlDat = new SqlDataAdapter(SqlCmd);
+                SqlDat.Fill(DtResultado);
+            }
+            catch (Exception ex)
+            {
+                DtResultado = null;
+
+            }
+            return DtResultado;
+        }
+
+        //Buscar Fechas
+        public DataTable BuscarFechas(string _textobuscar, string _textobuscar2)
+        {
+            DataTable DtResultado = new DataTable("Venta");
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                SqlCon.ConnectionString = Conexion.Cn;
+                SqlCommand SqlCmd = new SqlCommand();
+                SqlCmd.Connection = SqlCon;
+                SqlCmd.CommandText = "spbuscar_venta_fecha";
+                SqlCmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter ParTextoBuscar = new SqlParameter();
+                ParTextoBuscar.ParameterName = "@textobuscar";
+                ParTextoBuscar.SqlDbType = SqlDbType.VarChar;
+                ParTextoBuscar.Size = 20;
+                ParTextoBuscar.Value = _textobuscar;
+                SqlCmd.Parameters.Add(ParTextoBuscar);
+
+                SqlParameter ParTextoBuscar2 = new SqlParameter();
+                ParTextoBuscar2.ParameterName = "@textobuscar2";
+                ParTextoBuscar2.SqlDbType = SqlDbType.VarChar;
+                ParTextoBuscar2.Size = 20;
+                ParTextoBuscar2.Value = _textobuscar2;
+                SqlCmd.Parameters.Add(ParTextoBuscar2);
+
+                SqlDataAdapter SqlDat = new SqlDataAdapter(SqlCmd);
+                SqlDat.Fill(DtResultado);
+            }
+            catch (Exception ex)
+            {
+                DtResultado = null;
+
+            }
+            return DtResultado;
+        }
+
+
+        public DataTable MostrarDetalle(string _textobuscar)
+        {
+            DataTable DtResultado = new DataTable("Detalle_venta");
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                SqlCon.ConnectionString = Conexion.Cn;
+                SqlCommand SqlCmd = new SqlCommand();
+                SqlCmd.Connection = SqlCon;
+                SqlCmd.CommandText = "spmostrar_detalle_venta";
+                SqlCmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter ParTextoBuscar = new SqlParameter();
+                ParTextoBuscar.ParameterName = "@textobuscar";
+                ParTextoBuscar.SqlDbType = SqlDbType.VarChar;
+                ParTextoBuscar.Size = 20;
+                ParTextoBuscar.Value = _textobuscar;
+                SqlCmd.Parameters.Add(ParTextoBuscar);
+
+                SqlDataAdapter SqlDat = new SqlDataAdapter(SqlCmd);
+                SqlDat.Fill(DtResultado);
+            }
+            catch (Exception ex)
+            {
+                DtResultado = null;
+
+            }
+            return DtResultado;
+        }
+
+        //Mostrar artículos por nombre
+        public DataTable MostrarArticulo_venta_nombre(string _textobuscar)
+        {
+            DataTable DtResultado = new DataTable("Articulos");
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                SqlCon.ConnectionString = Conexion.Cn;
+                SqlCommand SqlCmd = new SqlCommand();
+                SqlCmd.Connection = SqlCon;
+                SqlCmd.CommandText = "spbuscararticulo_venta_nombre";
+                SqlCmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter ParTextoBuscar = new SqlParameter();
+                ParTextoBuscar.ParameterName = "@textobuscar";
+                ParTextoBuscar.SqlDbType = SqlDbType.VarChar;
+                ParTextoBuscar.Size = 20;
+                ParTextoBuscar.Value = _textobuscar;
+                SqlCmd.Parameters.Add(ParTextoBuscar);
+
+                SqlDataAdapter SqlDat = new SqlDataAdapter(SqlCmd);
+                SqlDat.Fill(DtResultado);
+            }
+            catch (Exception ex)
+            {
+                DtResultado = null;
+
+            }
+            return DtResultado;
+        }
+
+        //Mostrar artículos por código
+        public DataTable MostrarArticulo_venta_codigo(string _textobuscar)
+        {
+            DataTable DtResultado = new DataTable("Articulos");
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                SqlCon.ConnectionString = Conexion.Cn;
+                SqlCommand SqlCmd = new SqlCommand();
+                SqlCmd.Connection = SqlCon;
+                SqlCmd.CommandText = "spbuscararticulo_venta_codigo";
+                SqlCmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter ParTextoBuscar = new SqlParameter();
+                ParTextoBuscar.ParameterName = "@textobuscar";
+                ParTextoBuscar.SqlDbType = SqlDbType.VarChar;
+                ParTextoBuscar.Size = 20;
+                ParTextoBuscar.Value = _textobuscar;
+                SqlCmd.Parameters.Add(ParTextoBuscar);
+
+                SqlDataAdapter SqlDat = new SqlDataAdapter(SqlCmd);
+                SqlDat.Fill(DtResultado);
+            }
+            catch (Exception ex)
+            {
+                DtResultado = null;
+
+            }
+            return DtResultado;
+        }
+    }
+}
